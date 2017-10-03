8124f27faf6d23a308925d57afa9803781971f38
Octavioyuna <32420879+Octavioyuna@users.noreply.github.com>
2017-10-03 22:53:19 +0200

    Create simple.php

---
+++ dev/test/simple.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Simple</title>
+</head>
+<body>
+
+<h1>Simplea</h1>
+<p>This is a paragraph.</p>
+
+ 
+ 
+ <p>php</p>
+ <?php include("p3.php") ?>
+  <p> / php</p>
+</body>
+</html>
