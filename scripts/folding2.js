577fe0a178add75c04948a5fefef2f08b05dc700
Octavioyuna <32420879+Octavioyuna@users.noreply.github.com>
2017-10-08 19:55:02 +0200

    Update folding2.js

--- scripts/folding2.js
+++ scripts/folding2.js
@@ -1,8 +1,12 @@
 
-
+ /*
 $(function (){
     $('li').click(function(event) {
         event.stopPropagation();
     $(event.target).children('ul').slideToggle();
     });
+});
+*/
+$('div>ul>li .dir').click(function() {
+    $(this).slideToggle();
 });
