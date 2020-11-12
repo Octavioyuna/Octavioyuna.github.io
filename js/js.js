3cd451c323ae89692b23665966a143e93fb294f3
Octavioyuna <32420879+Octavioyuna@users.noreply.github.com>
2020-11-12 14:05:56 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -64,3 +64,8 @@ function writeFooterDivs() {
 }
 
 
+// When the user clicks on the button, scroll to the top of the document
+function goToTopFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
